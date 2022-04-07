3ace5855b5a67bf79e38375c2c7f3291bd357849
rolodoom <rolodoom@gmail.com>
2022-04-06 19:53:34 -0500

    Agregar Sesión 03 - Ejercicio1

---
+++ 02 Python/Tema 03 -  Introducción a los tipos de objetos/Ejercicio1.py
@@ -0,0 +1,26 @@
+# Declaración de variables
+nombre = "Joao"
+apellidos = "Pinto"
+edad = 30
+email = "info@joaopinto.net"
+telefono = 47854123
+casado = False
+tiene_hijos = True
+listado_amigos = ['Ademar Quieroz', 'Adeline Silva']
+peliculas_vistas = {
+    'peli1':'Morbius',
+    'peli2':'Spider-Man: No Way Home',
+    'peli3':'Duna',
+    'peli4':"Don't Look Up"
+}
+
+# Mostrar variables
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(tiene_hijos)
+print(listado_amigos)
+print(peliculas_vistas)
