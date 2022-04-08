bbcda6c6e0fd58b65be32bf842c3e452614ea37f
rolodoom <rolodoom@gmail.com>
2022-04-08 15:20:09 -0500

    Agregar Sesión 04 - Ejercicio3

---
+++ 02 Python/Tema 04 - Control de Flujo/Ejercicio3.py
@@ -0,0 +1,8 @@
+# Programa que muestra los números de 1 a 100
+# en orden inverso
+# Se usa la funcion range(start, stop, step)
+# para generar la lista
+# https://www.w3schools.com/python/ref_func_range.asp
+#
+for numero in range(100, 0, -1):
+    print(numero)
