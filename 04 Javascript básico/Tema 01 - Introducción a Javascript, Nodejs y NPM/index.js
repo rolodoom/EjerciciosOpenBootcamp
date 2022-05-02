ba8571b8f21e2fad7b76f366e70d1c37beeb155c
rolodoom <rolodoom@gmail.com>
2022-05-02 15:30:44 -0500

    Agregar Sesión 1 - Ejercicio1

---
+++ 04 Javascript básico/Tema 01 - Introducción a Javascript, Nodejs y NPM/index.js
@@ -0,0 +1,24 @@
+/*
+
+ _______   _______
+|  _____| |  ___  |
+| |       | |   | |    Rolando Ramos Torres (@rolodoom)
+| |       | |___| |    https://github.com/rolodoom
+|_|       |_______|
+ _         _______  
+| |       |  ___  |    Curso: Javascript básico
+| |       | |   | |    Sesion 1 - Introducción
+| |_____  | |___| |    Ejercicio 1
+|_______| |_______|
+
+
+Crea un archivo JavaScript donde muestres
+por consola la siguiente frase: 
+“Estoy realizando el curso de JavaScript, 
+espero aprender mucho”.
+
+*/
+
+'use strict';
+
+console.log('Estoy realizando el curso de JavaScript, espero aprender mucho');
