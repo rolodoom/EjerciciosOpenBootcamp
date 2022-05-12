fed08f4b64598441372431b07484b7afca308b1a
rolodoom <rolodoom@gmail.com>
2022-05-12 15:14:05 -0500

    Agregar Sesión 7 - Ejercicio 4 - Chrome

---
+++ 04 Javascript básico/Tema 07 - Sets y Objetos/chrome.js
@@ -0,0 +1,21 @@
+/*
+ _______   _______
+|  _____| |  ___  |
+| |       | |   | |    Rolando Ramos Torres (@rolodoom)
+| |       | |___| |    https://github.com/rolodoom
+|_|       |_______|
+ _         _______  
+| |       |  ___  |    Curso: Javascript básico
+| |       | |   | |    Sesion 7 - Sets y Objetos
+| |_____  | |___| |    Ejercicio 4 - chrome.js
+|_______| |_______|    
+
+- Abre una nueva ventana "about:blank" en Google Chrome
+- Abre la consola de desarrollador de Google Chrome (F12)
+- Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+*/
+"use strict"
+
+const edad = prompt("¿Cuál es tu edad?")
+console.log(edad)
