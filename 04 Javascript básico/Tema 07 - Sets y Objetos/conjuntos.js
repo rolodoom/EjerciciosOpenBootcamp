3e534503910dd27bb58aa62d64558ca942fe4ace
rolodoom <rolodoom@gmail.com>
2022-05-12 14:57:34 -0500

    Agregar Sesión 7 - Ejercicio 1 - Conjuntos

--- 04 Javascript básico/Tema 07 - Sets y Objetos/conjuntos.js
+++ 04 Javascript básico/Tema 07 - Sets y Objetos/conjuntos.js
@@ -7,26 +7,6 @@
  _         _______  
 | |       |  ___  |    Curso: Javascript básico
 | |       | |   | |    Sesion 7 - Sets y Objetos
-| |_____  | |___| |    Ejercicio 1 -  Conjuntos
-|_______| |_______|    
-
-Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
-- Un nuevo Set con los nombres de tu familia
-- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
-- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
-
-*/
-"use strict"
-
-/*
- _______   _______
-|  _____| |  ___  |
-| |       | |   | |    Rolando Ramos Torres (@rolodoom)
-| |       | |___| |    https://github.com/rolodoom
-|_|       |_______|
- _         _______  
-| |       |  ___  |    Curso: Javascript básico
-| |       | |   | |    Sesion 7 - Sets y Objetos
 | |_____  | |___| |    Ejercicio 1 - conjuntos.js
 |_______| |_______|    
 
